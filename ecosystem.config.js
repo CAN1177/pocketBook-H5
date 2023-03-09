42c716c0c0ff236d3ef48358fafa122dc801ed8a
Silva <39960526+CAN1177@users.noreply.github.com>
2023-03-09 11:30:49 +0800

    feat: pm2部署

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pocketBook-vite-h5',
+      script: 'pocketBook-vite-h5-server.js'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '1.116.55.82',
+      ref: 'origin/master',
+      repo: 'git@github.com:CAN1177/pocketBook-H5.git',
+      path: '/workspace/pocketBook-vite-h5',
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
